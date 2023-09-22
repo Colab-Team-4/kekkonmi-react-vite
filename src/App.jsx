7ba7fd2233a21e770d76ac5d9ee9f2b8a5d62bab
Hyun-Tae Jin <htjmario@gmail.com>
2023-09-22 05:21:29 -0400

    added tailwind and prettier as dependencies (#1)

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 export default function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1>Kekkonmi</h1>
-    </>
-  )
+    <div className="flex h-screen w-screen flex-col items-center justify-center bg-slate-800">
+      <h1 className="text-3xl text-slate-50">Kekkonmi</h1>
+    </div>
+  );
 }
-
