a7a5e327e0c7b1b137021f7b2eaa1b6439cb29d3
Hyun-Tae Jin <htjmario@gmail.com>
2023-10-07 02:54:38 -0400

    pushing up index.css changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -55,10 +55,10 @@ function Navbar() {
         </div>
 
         <div className="hidden bg-white lg:flex lg:gap-4">
-          <button type="button" className="btn-solid w-40 py-2">
+          <button type="button" className="btnSolid w-40 py-2">
             Sign Up
           </button>
-          <button type="button" className="btn-outline w-40">
+          <button type="button" className="btnOutline w-40">
             Log In
           </button>
 
