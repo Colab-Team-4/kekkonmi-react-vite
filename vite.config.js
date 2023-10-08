ac61f56803e1f80d035c51484c7e95f52e3fc854
Hyun-Tae Jin <htjmario@gmail.com>
2023-10-08 06:47:22 -0400

    vercel configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/", // set the base path
+  build: {
+    outDir: "dist", // specify the output directory
+  },
 });
